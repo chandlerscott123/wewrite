9838b1ed9be0891378d34d6e1b46937db1882ff2
Genesis Scott <chandlerscott123@gmail.com>
2023-03-11 17:18:24 -0600

    add new implmtn of getPriceFromCart, infinite loop. WIP.

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ const PRICE_COVER_ARR = [4,7,13];
 
 const EBOOK_PRICE = 25;
 
-CART_ID_ARR = ["poetry-book", "digital-anthology"]
+const CART_ID_ARR = ["poetry-book", "digital-anthology"]
 
 
 $(document).ready(function(){
@@ -18,83 +18,30 @@ $('.header').height($(window).height());
 });
 
 
-/*
-
-**** TO DO: ******
-
-
-
-
-ADD: onclick event listener: 
-        stored val cart in array accross diff webpages 
-        
-      items: items[name, quantity, price]
-                                    nameArray                          priceStr           quantityStr
-      cart.items.checkout();
-
-      PARAMETERS: [ [size , bookType , bookMaterial], [priceSize, priceMaterial], quantity ]
-
-      cart = array [nameArray, priceStr, quantityStr]
-      getPrice(): "small.blank.leather"
-
-
-const cart = {
-firstName: "John",
-lastName: "Doe",
-age: 50,
-status: "marketing contact"
-};
-
-                                
-
-poetry book size:
-small medium large
-
-input radio
-
-input radio
-
-input radio
-
-small (10-20 pages): 10
-medium(20-30 pages): 15
-large (30-50 pages): 25
-
-poetry book type:
-
-blank (FREE)
-filled (+ $12)
-
-poetry book of type with material:
-
-leather: +$13
-cardstock: +$7
-paper: +$4
-
-*/
-
-function setPriceInstance(){
-  let price;
+function setPoetryPriceInstance(){
+  let price=0;
   const size = document.querySelector("input[name='sizeRadio']:checked").value;
   const type = document.querySelector("input[name='typeRadio']:checked").value;
   const cover = document.querySelector("input[name='coverRadio']:checked").value;
+  let ebookQty =parseInt(document.querySelector("input#ebookQtyInput").value);
 
   let priceArr = [size,type,cover];
   
   for (let i=0; i<priceArr.length; i++){
-    if(i=0)
+    if(i===0)
     {
       price+=PRICE_SIZE_ARR[SIZE_ARR.indexOf(size)];
     }
-    else if(i=1)
+    else if(i===1)
     {
       price+= PRICE_TYPE_ARR[TYPE_ARR.indexOf(type)];
     }
-    else if (i=2)
+    else if (i===2)
     {
       price+= PRICE_COVER_ARR[COVER_ARR.indexOf(cover)];    
     }
   }
+  
   return price;
 }
 
@@ -103,20 +50,55 @@ function getPriceFromCart(cartIdArr){
   let finalPrice=0;
   let quant;
   let itemId;
-  let poetryPrice=setPriceInstance();
+  let poetryPrice=setPoetryPriceInstance();
+  let isNum = 1;
+  let index=0;
+  let lastNumIndex=0;
 
   if(cartIdArr.length>1){
-    for(i=0; i<cartIdArr.length; i++){
-      quant=parseInt(cartIdArr[i].substring(0,1));
-      itemId=cartIdArr[i].substring(1);
-      finalPrice += quant;
+  
+    //it's interating through the loop but doesn't have separate values for each index
+    //2item,3item
 
+    for(let i=0; i<cartIdArr.length; i++){
+      console.log("index in for loop: "+index); 
+      console.log("evaluating for" + cartIdArr[i]);
+     
+      while (isNum) //12poetry-book index: 0
+      {
+        quant=parseInt(cartIdArr[i].substring(0,index+1)); //1 index: 0 12 index: 1
+
+        if((quant>=10)){ 
+          index++; //index: 2
+          console.log("index in quant>=10 for quant " + quant.toString());
+          lastNumIndex=index; //index: 2
+          isNum = parseInt(cartIdArr[i].substring(0,index+1)); //12p -> NaN index:2
+        }
+
+        else if ((quant<10)){
+          lastNumIndex=0;
+          index++; 
+          console.log("index in quant<10 for quant " + quant.toString());
+          isNum = parseInt(cartIdArr[i].substring(0,index+1)); //12
+        } 
+      }
+      itemId=cartIdArr[i].substring(lastNumIndex); //poetry-book
+      console.log(itemId);
+      console.log(i);
+      if (itemId === CART_ID_ARR[0])
+      {
+        finalPrice+= (quant*poetryPrice); // 12 * price
+      }
+      else if (itemId === CART_ID_ARR[1])
+      {
+        finalPrice+=(quant*EBOOK_PRICE)
+      }
     }
   }
-  else if (cartIdArr>0) {
+  else if (cartIdArr.length===1) {
 
-    quant=parseInt(cartIdArr[i].substring(0,1));
-    itemId=cartIdArr[i].subtring(1);
+    quant=parseInt(cartIdArr[0].substring(0,1));
+    itemId=cartIdArr[0].substring(1);
     if (itemId === CART_ID_ARR[0])
     {
       finalPrice+= (quant*poetryPrice);
@@ -128,19 +110,20 @@ function getPriceFromCart(cartIdArr){
     
   }
 
+  document.getElementById("total-price").innerText = finalPrice;
+
+
 }
 
-function inCart()
+function processCart(event)
 {
   let cart =[];
-  let poetryInCart=false;
-  let ebookInCart=false;
   let poetryQty = parseInt(document.querySelector("input#poetryQtyInput").value);
   let ebookQty = parseInt(document.querySelector("input#ebookQtyInput").value);
 
   cart = [poetryQty, ebookQty];
 
-  for (i=0;i<cart.length; i++){
+  for (let i=0;i<cart.length; i++){
     if(cart[i]!==0)
     {
       if(i===0){
@@ -153,13 +136,22 @@ function inCart()
     }
   }
   getPriceFromCart(cart);
+  event.preventDefault();
 }
 
 
-window.addEventListener("load", function(){
-  let poertyForm = document.getElementById("poetryForm");
+function onSubmitPoetryForm(event){
+  event.preventDefault();
+  setPoetryPriceInstance();
 
-  // poertyForm.addEventListener("submit", )
+}
+
+window.addEventListener("load", function(){
+  let poetryForm = document.getElementById("poetryForm");
+  let totalForm = document.getElementById("get-total");
+
+  poetryForm.addEventListener("submit", onSubmitPoetryForm);
+  totalForm.addEventListener("submit", processCart);
   
 
 
